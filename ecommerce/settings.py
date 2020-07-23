e38f01a970e88bcc3bbc13907f33e26507e5b772
Lukasz <lukaszkkoziel@gmail.com>
2020-07-23 16:56:36 +0900

    AWS-PSQL integrated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -135,10 +135,12 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
+AWS_ACCESS_KEY_ID = 'AKIARKVLC7KIEMRDD7XF'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'wall-warket-django-app'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
