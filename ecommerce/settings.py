440368125b62bb7b29c7ce5bf1d93db47623b51d
Lukasz <lukaszkkoziel@gmail.com>
2020-07-29 12:32:58 +0900

    log in log out

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,6 +124,7 @@ USE_L10N = True
 USE_TZ = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'store'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
