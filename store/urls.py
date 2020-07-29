98546aa82fa8a3207616338a250dfbd51a83da57
Lukasz <lukaszkkoziel@gmail.com>
2020-07-29 11:49:37 +0900

    registaration

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-
+from users import views as user_views
 from . import views
 
 urlpatterns = [
   path('', views.store, name="store"),
+  path('register/', user_views.register, name="register"),
   path('cart/', views.cart, name="cart"),
   path('checkout/', views.checkout, name="checkout"),
 
   path('update_item/', views.updateItem, name="update_item"),
   path('process_order/', views.processOrder, name="process_order"),
-
+  path('register/', user_views.register, name="register"),
 ]
