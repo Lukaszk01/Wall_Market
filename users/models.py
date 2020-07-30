f323596cb5ab198b6a997d0d4064bc069a007d12
Lukasz <lukaszkkoziel@gmail.com>
2020-07-30 12:44:06 +0900

    profile configurations

--- users/models.py
+++ users/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='white-t-shirt-500x500.jpg', upload_to='images')
+    # image = models.ImageField(default='white-t-shirt-500x500.jpg', upload_to='images')
+
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
